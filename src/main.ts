2faed0a3ba1d2300827f2ddbb6a77bf079c1b46f
Anónio Teca <antonioteca@hotmail.com>
2026-04-09 19:43:34 +0100

    feat: add Swagger documentation setup for API with JWT authentication support

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module'; // Onde os controladores e serviços são definidos.
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 // Ponto de entrada da aplicação,
 // onde o aplicativo é criado e configurado para ouvir as solicitações na porta especificada (padrão 3000).
@@ -12,6 +13,27 @@ async function bootstrap() {
   app.enableCors({
     origin: '*', // Permite solicitações de qualquer origem. Em produção, é recomendado restringir isso a domínios específicos.
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Church SaaS API')
+    .setDescription('Documentação da API para os Membros B, C e D')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Insira o seu token JWT',
+        in: 'header',
+      },
+      'access-token', // Este é o nome da referência para usarmos nos controllers
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();

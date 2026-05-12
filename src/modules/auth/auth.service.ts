16ae7ffa8b3fb814b3b9f968e62e5fd38c8b7716
txavier <tchiadeestevaox@gmail.com>
2026-05-12 17:25:50 +0100

    A lógica de login com email e password foi agora implementada de forma básica, esta funcional mas ainda não está tão robusta

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/login.dto';
+import { compare, hash } from 'bcrypt'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(
+		private prisma: PrismaService,
+		private jwtService: JwtService,
+	){}
+
+	private async generateToken(userId: string, email: string)
+	{
+		const payload = { sub: userId, email: email };
+		return {
+			access_token: this.jwtService.sign(payload),
+		};
+	}
+	async loginWithEmailAndPassword(loginDto: LoginDto) : Promise<any | null>
+	{
+		const { email, password } = loginDto;
+
+		const user = await this.prisma.user.findUnique({
+			where: {email},
+			select: {
+				id: true,
+				email: true,
+				displayName: true,
+				passwordHash: true,
+			},
+		});
+		
+		if (!user || !user.passwordHash)
+			throw new UnauthorizedException('Credenciais inválidas');
+		
+		const isvalidPW = await compare(password!, user.passwordHash);
+
+		if (!isvalidPW)
+			throw new UnauthorizedException('Credenciais inválidas');
+		return{
+			success: true,
+			email: user.email,
+		};
+	}
+}
+

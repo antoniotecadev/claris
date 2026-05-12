c88a276f24afc3889a95dbb0d2e8f362f3524160
txavier <tchiadeestevaox@gmail.com>
2026-05-12 13:27:14 +0100

    iniciei a criação do modulo do auth, nesse arquivo eu defini os controller e os providers, é como o nó central aonde tudo se cruza (está em fase inicial)

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+	imports: [
+		PassportModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: async (configService: ConfigService) => ({
+				secret: configService.get<string>('JWT_SECRET'),
+				signOptions: { expiresIn: '1d' }, 
+			}),
+		}),
+		PrismaModule,
+	],
+	controllers: [AuthController],
+	providers: [AuthService],
 })
 export class AuthModule {}

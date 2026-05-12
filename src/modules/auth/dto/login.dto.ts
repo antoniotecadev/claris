7faa5c8fd3a3b6ea6563598d46b7952f027178da
txavier <tchiadeestevaox@gmail.com>
2026-05-12 14:30:24 +0100

    dto do login criado

---
+++ src/modules/auth/dto/login.dto.ts
@@ -0,0 +1,11 @@
+import { isEmail, IsEmail, IsNotEmpty, isNotEmpty, IsString, MinLength, minLength  } from "class-validator";
+
+export class LoginDto{
+	@IsEmail({}, { message: 'Email inválido' })
+	@IsNotEmpty({ message: 'O email é obrigatório' })
+	email: string | undefined
+
+	@IsString()
+	@IsNotEmpty({ message: 'A senha é obrigatória' })
+	@MinLength(6, { message: '' })
+}

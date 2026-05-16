a70bec1cdef26f4b97591cbdc4db2d1583da4c43
txavier <tchiadeestevaox@gmail.com>
2026-05-16 17:43:50 +0100

    feat: implement Google authentication routes in AuthController

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Body, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
-export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+export class AuthController 
+{
+	constructor(private readonly authService: AuthService)
+	{}
+
+	@Post('register')
+	async register(@Body() registerDto: RegisterDto)
+	{
+		return this.authService.registerWithEmail(registerDto);
+	}
+
+	@Post('login')
+	async login(@Body() loginDto: LoginDto)
+	{
+		return this.authService.loginWithEmailAndPassword(loginDto);
+	}
+
+	@Get('google')
+	@UseGuards(AuthGuard('google'))
+	async googleLogin()
+	{}
+
+	@Get('google/callback')
+	@UseGuards(AuthGuard('google'))
+	async googleLoginCallback(@Req() req: any)
+	{
+		return await this.authService.loginWithGoogle(req.user);
+	}
 }

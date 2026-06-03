c7764068475de1004e0d1825fd6840048079f94b
txavier <tchiadeestevaox@gmail.com>
2026-06-03 14:25:04 +0100

    função para geral o código de verificação enviado no email criada

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { LoginDto } from './dto/login.dto';
@@ -6,13 +6,19 @@ import { RegisterDto } from './dto/register.dto'
 import { JwtPayload } from './interfaces/jwt-payload.interface'
 import { UserPayload } from './interfaces/user-payload.interface';
 import { compare, hash } from 'bcrypt'
+import { Resend } from 'resend';
+import { ConfigService } from '@nestjs/config';
+import { randomInt } from 'node:crypto';
 
 
 @Injectable()
 export class AuthService {
+	private readonly resend = new Resend(process.env.RESEND_API_KEY ?? '');
+	private readonly resendFrom = process.env.RESEND_FROM ?? 'claris@resend.dev'
 	constructor(
 		private prisma: PrismaService,
 		private jwtService: JwtService,
+		private configService: ConfigService,
 	){}
 
 	async registerWithEmail(registerDto: RegisterDto) : Promise<any | null>
@@ -52,7 +58,7 @@ export class AuthService {
 	private async generateFinalLoginResponse(user: UserPayload)
 	{
 		const loginPayload: JwtPayload = {
-			id: user.id!,
+			id: user.id,
 			email: user.email!,
 		};
 
@@ -107,10 +113,44 @@ export class AuthService {
 
 		if (!isvalidPW)
 			throw new UnauthorizedException('Credenciais inválidas');
+
 		return{
 			success: true,
 			email: user.email,
 		};
+	}
+
+	private generateEmailCode(){
+		return randomInt(0, 1_000_000).toString().padStart(6, '0');
+	}
+
+	private async sendLoginEmailCode(user: UserPayload)
+	{
+		const RESEND_API_KEY = this.configService.get<string>('RESEND_API_KEY')
+
+		if (!RESEND_API_KEY)
+			throw new Error('RESEND_API_KEY is not defined');
+		
+		const lastcode = await this.prisma.emailLoginCode.findFirst({
+			where: {userId: user.id},
+			orderBy: {createdAt: 'desc'}
+		});
+
+		if (lastcode){
+			const elapsedMs = Date.now() - lastcode.createdAt.getTime();
+			const cooldownMs = 5 * 60 * 1000;
+
+			if (elapsedMs < cooldownMs){
+				const waitSeconds = Math.ceil((cooldownMs - elapsedMs) / 1000);
+				throw new BadRequestException({
+					message: `Aguarde ${waitSeconds}s par reenviar o código`,
+					waitSeconds,
+					statusCode: 400,
+					error: 'Bad Request',
+				});
+			}
+		}
+		const code = this.
 	}
 
 	async loginWithGoogle(googleUser: any): Promise<any | null>
